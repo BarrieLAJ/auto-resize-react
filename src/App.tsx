9c62b32b31d3c418d0d6939387882b8bbfe62930
BarrieLAJ <alhajiab.barrie@gmail.com>
2021-01-27 10:26:31 +0000

    Styled with Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 //import logo from './logo.svg';
 //import './App.css';
-import { Tile } from "./features/AutoSize";
+import  Tile from "./features/AutoSize";
+
 
 const App = () => {
   // const elemRef = React.useRef(null);
@@ -13,14 +14,22 @@ const App = () => {
   // },[])
   return (
     <>
-      <div>This is a test</div>
-      <div>This is a test</div>
-      <div>This is a test</div>
-      <Tile>
-        {({ width, height,}) => (
+      <Tile scrollBarProp = {{}}>
+        {({ width, height, className}) => (
           <div
-            style={{ height, width, backgroundColor: "darkgoldenrod" }}
-          ></div>
+            style={{ height, width}}
+            className={className}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet consectetur urna. Proin malesuada risus sed est hendrerit lacinia. In at nisi posuere metus finibus ornare sed vel enim. Etiam vitae pellentesque tortor, ac gravida risus. Nam ut efficitur erat. Vestibulum vestibulum elit sit amet urna viverra, mattis dapibus leo rutrum. Pellentesque eleifend nunc eget est molestie, quis finibus ligula vehicula. Vivamus sit amet tortor pharetra, lacinia mi et, volutpat turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+Ut quis diam eu augue tempus accumsan ultricies a diam. Praesent at tellus purus. Aliquam lacinia suscipit magna in feugiat. Integer malesuada nisi libero, ut fermentum tortor viverra at. Fusce id nibh placerat, pretium nibh sed, vehicula justo. Sed semper, dolor rutrum facilisis suscipit, turpis lorem pretium elit, et interdum turpis tortor quis justo. Etiam eget placerat turpis. Nulla vel cursus mi. Donec porta velit sapien, ac bibendum dui tempor vitae. Suspendisse sit amet convallis justo. Integer diam risus, finibus ut tincidunt euismod, bibendum eget lectus. Nam suscipit interdum nulla ac vestibulum. Sed vitae magna sit amet nisi mattis volutpat.
+
+Nunc a malesuada dolor. In hac habitasse platea dictumst. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi interdum ut sem in condimentum. Sed tempor feugiat metus, vel porta ligula dapibus sit amet. Fusce ac ex vel sapien sollicitudin lobortis vitae id mauris. Etiam quis risus ac leo viverra fermentum. Nulla tempus sem non ligula feugiat, non maximus neque pellentesque. Phasellus sem metus, interdum at porta sed, tempor ac ipsum. Fusce risus sapien, imperdiet eget luctus at, fermentum non ligula.
+
+Nulla vulputate nisl non auctor sodales. Nunc nulla nunc, egestas sit amet ultrices nec, lobortis a lorem. Praesent varius, nunc mattis bibendum imperdiet, nulla enim molestie lorem, non tempor sapien augue sed lectus. Vivamus et purus condimentum quam laoreet gravida. Nam fringilla eu risus id vulputate. Praesent ultricies sapien quis dolor aliquam, ac iaculis tellus rutrum. Vestibulum pellentesque, ante quis viverra interdum, enim nulla viverra enim, ullamcorper dapibus nisi velit id urna. Nulla porttitor sodales ex sit amet faucibus. Praesent sed fringilla dui. Phasellus a gravida nulla. Morbi finibus lectus sed lacus fringilla, quis lacinia ipsum facilisis. Phasellus pretium aliquet faucibus. Ut scelerisque vel tellus et euismod. Praesent maximus, turpis sed suscipit viverra, odio diam porttitor ipsum, eu pharetra nisi augue sed orci. Mauris ultricies velit erat, in dignissim nibh pharetra iaculis. Vestibulum tristique pellentesque magna id bibendum.
+
+Mauris lacinia imperdiet massa id viverra. Sed nec sagittis dolor, sit amet iaculis mauris. Donec ullamcorper nec metus id blandit. Vivamus condimentum ante et interdum tempus. Phasellus aliquet condimentum pulvinar. Cras tempus semper nibh, vestibulum sodales elit iaculis quis. Donec sollicitudin mattis interdum. Vivamus vestibulum, diam nec ornare vulputate, orci arcu tincidunt nulla, gravida placerat nunc enim a erat. Pellentesque a massa quis neque ullamcorper consectetur eu id leo. Quisque vehicula malesuada augue id vulputate. Vestibulum sit amet risus lobortis, pellentesque tellus vitae, auctor massa. Aenean vitae fermentum sem, tristique aliquam tortor. Proin at tellus nulla.
+          </div>
         )}
       </Tile>
       </>
--- src/features/AutoSize.tsx
+++ src/features/AutoSize.tsx
@@ -1,82 +1,97 @@
-import React, { useState, useEffect, ReactNode, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  ReactNode,
+  useRef,
+  useCallback,
+} from "react";
+import styled from "styled-components";
 
-export const Tile = (props: {
-  children: ({ width, height }: { width: number; height: number }) => ReactNode;
+const TopElem =styled.div`
+height: 0;
+background-color: red;
+`
+
+const BottomElem = styled.div`
+position: fixed;
+bottom: 0;
+right: 0;
+height: 0;
+background-color: green;
+z-index: -1;
+`
+
+const Tile = (props: {
+  scrollBarProp?: {}
+  className?: any
+  children: ({ width, height }: { width: number; height: number, 
+    className: string
+  }) => ReactNode;
 }) => {
   const topElem = useRef<any>(null);
   const bottomElem = useRef<any>(null);
   const [height, setHeight] = useState<number>();
   const [width, setWidth] = useState<number>();
-  const [refIsSet, setRefIsSet] = useState(false);
-  const resizeobserver = useRef<ResizeObserver | null>();
-  const refIsTrue = refIsSet === true;
+  const lastTimerRef = useRef<any>(-1)
 
-  useEffect(() => {
-    setRefIsSet(true);
-  }, []);
-
-
-
-//resize effect
-  useEffect(() => {
-    resizeobserver.current = new ResizeObserver((entries, observer) => {
-      for (let entry of entries) {
-        if (
-          entry.contentRect.right
-        ) {
-          let topRect = topElem.current.getBoundingClientRect();
-          let bottomRect = bottomElem.current.getBoundingClientRect();
-          let height = bottomRect.top - topRect.top;
-          let width = bottomRect.left - topRect.left;
-          setHeight(height);
-          setWidth(width);
-        }
-      }
-    });
-    if (refIsSet) {
-      resizeobserver.current.observe(topElem.current);
-    }
-    return () => {
-      if (resizeobserver.current) {
-        resizeobserver.current.disconnect();
-      }
-    };
-  }, [resizeobserver, refIsSet]);
-
-
-  //re-render after first mount
-  useEffect(() => {
+  const computeDimension = useCallback(() => {
     let topRect = topElem.current.getBoundingClientRect();
     let bottomRect = bottomElem.current.getBoundingClientRect();
     let height = bottomRect.top - topRect.top;
     let width = bottomRect.left - topRect.left;
     setHeight(height);
     setWidth(width);
-  }, [refIsTrue]);
+  }, []);
 
+  // let style:CSSProperties = {}
+
+  // if(props.scrollBarProp){
+  //   style = {
+  //     overflowY: "auto"
+  //   }
+  // }
+
+  //resize effect
+  useEffect(() => {
+    const resizeobserver: ResizeObserver = new ResizeObserver(
+      (entries) => {
+        clearTimeout(lastTimerRef.current)
+        const timer = setTimeout(() => {
+          computeDimension();
+       }, 30);
+        lastTimerRef.current = timer
+      }
+    );
+    resizeobserver.observe(topElem.current);
+    return () => {
+      resizeobserver.disconnect();
+    };
+  }, [computeDimension]);
+
+  //re-render after first mount
+  console.log(props.className)
   return (
     <>
-      <div
+      <TopElem
         ref={topElem}
-        style={{
-          height: 2,
-          backgroundColor: "red",
-        }}
       />
-      <div
+      <BottomElem
         ref={bottomElem}
-        style={{
-          position: "fixed",
-          bottom: 0,
-          right: 0,
-          height: 1,
-          backgroundColor: "green",
-          zIndex: 100,
-        }}
       />
       {width !== undefined && height !== undefined
-        ? props.children({ width, height })
+        ? props.children({ width, height, className:props.className })
         : null}
     </>
   );
 };
+
+
+export default styled(Tile)`
+  overflow-y: ${props => props.scrollBarProp ? "auto" : "hidden"};
+  overflow-x: hidden;
+  background-color: darkgoldenrod;
+`
+
+
+
+//styled.div``
